77ead491f56d3d6a1dedc7c2b7481d7dbb04a952
Marianna <mckd2018@gmail.com>
2018-10-30 18:18:49 -0700

    add MongoDB and Review model

--- app.js
+++ app.js
@@ -1,27 +1,47 @@
 // Express - define routes
 const express = require('express');
+
 //MIDDLEWARE - plugins or libraries we use to extend a web framework
 var exphbs = require('express-handlebars');
 
 // Create an object in express
 const app = express();
 
+// Connect to Database via MONGOOSE
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/rotten-potatoes');
+
+/*
+Create Model/Data Layer - where you put the code dedicated to interacting
+the database
+*/
+const Review = mongoose.model('Review', {
+    title: String,
+    movieTitle: String
+})
+
+// Create engine
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-
-
-
 // OUR MOCK ARRARY OF PROJECTS
-let reviews = [
-    { title: "Great Review", movieTitle: "Batman IT" },
-    { title: "Awesome Movie", movieTitle: "Titanic" },
-    { title: "Funny Movie", movieTitle: "Mall Cop" }
-]
+// let reviews = [
+//     { title: "Great Review", movieTitle: "Batman IT" },
+//     { title: "Awesome Movie", movieTitle: "Titanic" },
+//     { title: "Funny Movie", movieTitle: "Mall Cop" }
+// ]
 
 //ROOT ROUTE - INDEX
 app.get('/', (req, res) => {
-    res.render('reviews-index', { reviews: reviews });
+    Review.find()
+    // Provide a function for the Promise to call when it resolves- when it finished whatever it was doing.
+    .then(reviews => {
+        res.render('reviews-index', { reviews: reviews });
+    })
+    // Provide a function for the promise to call if it is rejected. A Promise is rejected if it fails.
+    .catch((err) => {
+        console.log(err);
+    })
 })
 
 /*
