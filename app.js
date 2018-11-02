c3ec1d938f705d89f135442b6bd467a0ae0c9d9e
Marianna <mckd2018@gmail.com>
2018-11-01 18:59:20 -0700

    Users can edit and update reviews and form fix

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
+// app.js
 // Express - define routes
 const express = require('express');
 
 //MIDDLEWARE - plugins or libraries we use to extend a web framework
-var exphbs = require('express-handlebars');
+const exphbs = require('express-handlebars');
 
 // INITIALIZE BODY-PARSER AND ADD IT TO APP
 const bodyParser = require('body-parser');
+
+// MIDDLEWARE
+const methodOverride = require('method-override');
 
 // Create an object in express
 const app = express();
@@ -16,6 +20,9 @@ app.set('view engine', 'handlebars');
 
 // Add Body Parser that allows express to see form data that is coming in from a POST request.
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// override with POST having ?_method=DELETE or ?_method=PUT
+app.use(methodOverride('_method'))
 
 // Connect to Database via MONGOOSE
 const mongoose = require('mongoose');
@@ -55,7 +62,8 @@ app.get('/', (req, res) => {
 //NEW
 app.get('/reviews/new', (req, res) => {
     res.render('reviews-new', {});
-})
+
+});
 
 //CREATE
 app.post('/reviews', (req, res) => {
@@ -74,9 +82,26 @@ app.get('/reviews/:id', (req, res) => {
     }).catch((err) => {
         console.log(err.message);
     })
-
 });
 
+// EDIT
+app.get('/reviews/:id/edit', (req, res) => {
+    Review.findById(req.params.id, function(err, review) {
+        res.render('reviews-edit', {review: review});
+    })
+});
+
+//UPDATE
+app.put('/reviews/:id', (req, res) => {
+    Review.findByIdAndUpdate(req.params.id, req.body)
+    .then(review => {
+        console.log(review);
+        res.redirect(`/reviews/${review._id}`) // Redirect to reviews/:id
+    })
+    .catch(err => {
+        console.log(err.message)
+    })
+});
 // Web Server Check
 app.listen(3000, () => {
     console.log('App listening on port 3000')
