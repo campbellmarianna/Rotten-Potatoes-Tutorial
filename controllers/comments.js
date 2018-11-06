ce075fe0104dac2f7d5491a1f4fa3a5d1b9c47e1
Marianna <mckd2018@gmail.com>
2018-11-05 19:18:59 -0800

    Users can delete a comment

--- controllers/comments.js
+++ controllers/comments.js
@@ -11,9 +11,17 @@ module.exports = (app) => {
      app.post('/reviews/comments', (req, res) => {
          Comment.create(req.body).then(comment => {
              res.redirect(`/reviews/${comment.reviewId}`);
-             console.log(comment)
          }).catch((err) => {
              console.log(err.message);
          });
      });
+
+     // DELETE
+     app.delete('/reviews/comments/:id', function (req, res) {
+         Comment.findByIdAndRemove(req.params.id).then((comment) => {
+             res.redirect(`/reviews/${comment.reviewId}`);
+         }).catch((err) => {
+             console.log(err.message);
+         })
+     })
 };
